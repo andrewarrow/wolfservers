408b110c8a50c745fd62d3e4a51f13a0af224e84
Andrew Arrow <oneone@gmail.com>
2021-04-30 06:14:23 -0700

    db

--- sqlite/query.go
+++ sqlite/query.go
@@ -1,5 +1,25 @@
 package sqlite
 
+import (
+	"fmt"
+)
+
 func NameExists(name string) bool {
+	db := OpenTheDB()
+	defer db.Close()
+	rows, err := db.Query("select name from stakes where name=?", name)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var s1 string
+		rows.Scan(&s1)
+		if s1 == name {
+			return true
+		}
+	}
 	return false
 }
