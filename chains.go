2d63d94663ba4f5aeb514b8bb12334e5f8428167
Andrew Arrow <oneone@gmail.com>
2021-05-09 19:47:21 -0700

    chains

--- chains.go
+++ chains.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
+// List of contenders for what will be _the_ proof-of-stake
+// blockchain other than ETH2.0
 func ListChains() {
 	fmt.Println("")
-	fmt.Println(" 1. ADA      : cardano.org  2017 Charles Hoskinson IOHK")
-	fmt.Println(" 2. ALGO     : algorand.org 2017 Silvio Micali ")
-	fmt.Println(" 3. : ")
-	fmt.Println(" 4. : ")
-	fmt.Println(" 5. : ")
+	fmt.Println(" 1. ADA      : cardano.org  2017 Charles Hoskinson")
+	fmt.Println(" 2. ALGO     : algorand.org 2017 Silvio Micali")
+	fmt.Println(" 3. BNB      : binance.com  2019 Smart Chain PoSA")
+	fmt.Println(" 4. XLM      : stellar.org  2015 McCaleb/Collison")
+	fmt.Println(" 5. XTZ      : tezos.com    2018 Arthur Breitman")
 	fmt.Println(" 6. : ")
 	fmt.Println(" 7. : ")
 	fmt.Println(" 8. : ")
